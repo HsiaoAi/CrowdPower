cf8bd3d1d51019b07ecd16c6333c6ff9435da95e
HsiaoAi <ilct227@gmail.com>
2019-05-05 10:18:16 +0200

    added AR videos

--- CrowdPower/AR/ARViewController.swift
+++ CrowdPower/AR/ARViewController.swift
@@ -51,7 +51,38 @@ class ARViewController: UIViewController, StoryboardInit {
     
     @objc func tapOnView() {
         
-        present(DetailViewController.storyboardInit(), animated: true)
+        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+            node.removeFromParentNode()
+        }
+        
+        guard let currentFrame = self.sceneView.session.currentFrame else {
+            return
+        }
+        
+        let videoNode = SKVideoNode(fileNamed: "Untitled.mp4")
+        videoNode.play()
+        
+        let skScene = SKScene(size: CGSize(width: 640, height: 480))
+        skScene.addChild(videoNode)
+        
+        videoNode.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
+        videoNode.size = skScene.size
+        
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = skScene
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.0
+        
+        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        tvPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+        
+//        present(DetailViewController.storyboardInit(), animated: true)
     }
     
     func configureLighting() {
